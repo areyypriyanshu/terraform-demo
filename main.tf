1ff3b2a1214a410b7f7f32724180a4c798ca8b52
areyypriyanshu <priyanshumandal63@gmail.com>
2026-02-06 11:38:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,24 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
 resource "aws_instance" "demo" {
-  ami           = "ami-0b0ea68c435eb488d" # Amazon Linux 2 (eu-north-1)
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
 
   tags = {
